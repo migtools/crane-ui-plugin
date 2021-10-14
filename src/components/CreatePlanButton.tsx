de138d04154be1f4cd88affc4ae81873d6126ed9
Ian Bolton <ibolton@redhat.com>
2021-10-14 12:25:09 -0400

    Add shared lib-ui components for react query

---
+++ src/components/CreatePlanButton.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { Button, ButtonProps } from "@patternfly/react-core";
+import { useHistory } from "react-router-dom";
+interface ICreatePlanButtonProps {
+  variant?: ButtonProps["variant"];
+}
+
+const CreatePlanButton: React.FunctionComponent<ICreatePlanButtonProps> = ({
+  variant = "primary",
+}: ICreatePlanButtonProps) => {
+  const history = useHistory();
+  return (
+    <Button
+      isSmall
+      onClick={() => history.push("/plans/create")}
+      isAriaDisabled={true}
+      variant={variant}
+      id="create-plan-button"
+    >
+      Create plan
+    </Button>
+  );
+};
+
+export default CreatePlanButton;
--- src/components/PlansList.tsx
+++ src/components/PlansList.tsx
@@ -1,23 +1,55 @@
+import { PageSection } from "@patternfly/react-core/dist/esm/components/Page/PageSection";
 import * as React from "react";
 import { usePlansQuery } from "./plans";
+import { ResolvedQuery } from "@konveyor/lib-ui";
+import { Card } from "@patternfly/react-core/dist/esm/components/Card/Card";
+import { CardBody } from "@patternfly/react-core/dist/esm/components/Card/CardBody";
+import { EmptyState } from "@patternfly/react-core/dist/esm/components/EmptyState/EmptyState";
+import spacing from "@patternfly/react-styles/css/utilities/Spacing/spacing";
+import { EmptyStateIcon } from "@patternfly/react-core/dist/esm/components/EmptyState/EmptyStateIcon";
+import { PlusCircleIcon } from "@patternfly/react-icons";
+import { Title } from "@patternfly/react-core/dist/esm/components/Title/Title";
+import { EmptyStateBody } from "@patternfly/react-core/dist/esm/components/EmptyState/EmptyStateBody";
+import CreatePlanButton from "./CreatePlanButton";
 
 const PlansList: React.FC = () => {
   const plansQuery = usePlansQuery();
-  console.log("plansQuery", plansQuery);
-  // const [data, loaded] = useK8sWatchResource<K8sResourceCommon[]>({
-  //   kind: 'Pod',
-  //   isList: true,
-  // });
   return (
-    <div>
-      Plans list:
-      {plansQuery?.data?.items.map((d) => (
-        <div key={d.metadata.name}>{d.metadata.name}</div>
-      ))}
-      {/* <div>The WebSocket is currently {connectionStatus}</div>
-      {lastJsonMessage ? <span>Last message: {lastJsonMessage?.data?.kind}</span> : null}
-      <ul></ul> */}
-    </div>
+    <PageSection>
+      <ResolvedQuery
+        result={plansQuery}
+        errorTitle={"Could not load plans"}
+        errorsInline={false}
+      >
+        <Card>
+          <CardBody>
+            {!plansQuery.data ? null : plansQuery.data.items.length === 0 ? (
+              <EmptyState className={spacing.my_2xl}>
+                <EmptyStateIcon icon={PlusCircleIcon} />
+                <Title size="lg" headingLevel="h2">
+                  No migration plans
+                </Title>
+                <EmptyStateBody>
+                  Create a migration plan to select clusters to migrate to{" "}
+                </EmptyStateBody>
+                <CreatePlanButton />
+              </EmptyState>
+            ) : (
+              <div>
+                Plans list:
+                {plansQuery?.data?.items.map((d) => (
+                  <div key={d.metadata.name}>{d.metadata.name}</div>
+                ))}
+              </div>
+              // <PlansTable
+              //   plans={plansQuery.data?.items || []}
+              //   errorContainerRef={errorContainerRef}
+              // />
+            )}
+          </CardBody>
+        </Card>
+      </ResolvedQuery>
+    </PageSection>
   );
 };
 
