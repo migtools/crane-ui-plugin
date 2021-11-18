e965f3d73da348e366dd97b5976222ca047f0760
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-11-18 14:07:53 -0500

    Upgrade dependencies, fix build

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,7 @@
 
 import * as webpack from 'webpack';
 import * as path from 'path';
-import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk/lib/index-webpack';
+import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
 const { stylePaths } = require('./stylePaths');
 
 const config: webpack.Configuration = {
