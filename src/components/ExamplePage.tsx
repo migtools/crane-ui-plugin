b20cc15175e39d7cc18f2da631572aa8bc4b5525
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-01-13 16:23:18 -0500

    Apply lint:fix

--- src/components/ExamplePage.tsx
+++ src/components/ExamplePage.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
-import {
-  Page,
-  PageSection,
-  Text,
-  TextContent,
-  TextVariants,
-  Title,
-} from '@patternfly/react-core';
+import { Page, PageSection, Text, TextContent, TextVariants, Title } from '@patternfly/react-core';
 
 export default function ExamplePage() {
   return (
@@ -22,16 +15,15 @@ export default function ExamplePage() {
         <PageSection variant="light">
           <TextContent>
             <Text component={TextVariants.p}>
-              This is a custom page contributed by the template console plugin.
-              The extension that adds the page is declared in
-              console-extensions.json in the package root along with the
-              corresponding nav item. Update console-extensions.json to change
-              or add extensions.
+              This is a custom page contributed by the template console plugin. The extension that
+              adds the page is declared in console-extensions.json in the package root along with
+              the corresponding nav item. Update console-extensions.json to change or add
+              extensions.
             </Text>
             <Text component={TextVariants.p}>
               After cloning this project, replace references to
-              <code>console-template-plugin</code> and other plugin metadata in
-              the <code>package.json</code> and <code>manifest.yaml</code>
+              <code>console-template-plugin</code> and other plugin metadata in the{' '}
+              <code>package.json</code> and <code>manifest.yaml</code>
               files with values for your plugin.
             </Text>
           </TextContent>
