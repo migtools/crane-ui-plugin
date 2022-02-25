dfa9cf8d847cc2bd66b3a15820081f8b1b5d177e
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-02-25 14:17:11 -0500

    Remove references to crane-proxy configmap, temporarily allow navigation without validating credentials (#32)
    * Remove references to crane-proxy configmap
    
    * Temporarily allow navigation without validating credentials

--- src/api/queries/secrets.ts
+++ src/api/queries/secrets.ts
@@ -3,19 +3,15 @@ import {
   useK8sModel,
   k8sList,
   k8sCreate,
-  k8sGet,
   k8sPatch,
   k8sDelete,
 } from '@openshift-console/dynamic-plugin-sdk';
 import { K8sModel } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
 import { useMutation } from 'react-query';
 import { useNamespaceContext } from 'src/context/NamespaceContext';
-import { isSameResource } from 'src/utils/helpers';
-import { ProxyConfigMap, ProxyConfigMapCluster } from '../types/ConfigMap';
 import { OAuthSecret, Secret } from '../types/Secret';
 
 export const secretGVK: K8sGroupVersionKind = { group: '', version: 'v1', kind: 'Secret' };
-export const configMapGVK: K8sGroupVersionKind = { group: '', version: 'v1', kind: 'ConfigMap' };
 
 interface UseConfigureSecretMutationArgs {
   existingSecretFromState: OAuthSecret | null;
@@ -31,7 +27,6 @@ export const useConfigureProxyMutation = ({
   onSuccess,
 }: UseConfigureSecretMutationArgs) => {
   const [secretModel] = useK8sModel(secretGVK);
-  const [configMapModel] = useK8sModel(configMapGVK);
   const namespace = useNamespaceContext();
   return useMutation<OAuthSecret, Error, ConfigureProxyMutationParams>(
     async ({ apiUrl, token }) => {
@@ -42,44 +37,19 @@ export const useConfigureProxyMutation = ({
         existingSecret = await findExistingSecret(secretModel, namespace, apiUrl, 'source');
       }
 
-      let secretToUse: OAuthSecret | null = null;
-      let deletedSecret: OAuthSecret | null = null;
-
-      // If we have an existing secret that doesn't match our credentials, delete it so we can replace it.
+      // If we have an existing secret that matches our credentials, we can just use it as-is.
       if (existingSecret) {
-        if (!secretMatchesCredentials(existingSecret, apiUrl, token)) {
-          await k8sDelete({ model: secretModel, resource: existingSecret });
-          deletedSecret = existingSecret;
-        } else {
-          // If it already matches, we can just use it as-is.
-          secretToUse = existingSecret;
+        if (secretMatchesCredentials(existingSecret, apiUrl, token)) {
+          return existingSecret;
         }
+        // If it doesn't match our credentials we delete it so we can replace it.
+        await k8sDelete({ model: secretModel, resource: existingSecret });
       }
 
-      if (!secretToUse) {
-        secretToUse = await k8sCreate<OAuthSecret>({
-          model: secretModel,
-          data: getNewSecret('source', namespace, apiUrl, token),
-        });
-      }
-
-      // Patch the proxy ConfigMap to add the new secret and remove the deleted secret if applicable
-      const proxyConfigMap = await k8sGet<ProxyConfigMap>({
-        model: configMapModel,
-        ns: 'openshift-migration',
-        name: 'crane-proxy',
+      return await k8sCreate<OAuthSecret>({
+        model: secretModel,
+        data: getNewSecret('source', namespace, apiUrl, token),
       });
-      let newClustersJSON = addSecretToClustersJSON(proxyConfigMap.data.clusters, secretToUse);
-      if (deletedSecret) {
-        newClustersJSON = removeSecretFromClustersJSON(newClustersJSON, deletedSecret);
-      }
-      await k8sPatch<ProxyConfigMap>({
-        model: configMapModel,
-        resource: proxyConfigMap,
-        data: [{ op: 'replace', path: '/data/clusters', value: newClustersJSON }],
-      });
-
-      return secretToUse;
     },
     { onSuccess },
   );
@@ -160,28 +130,6 @@ const getNewSecret = (
   },
   type: 'Opaque',
 });
-
-const parseClustersJSON = (clustersJSON: string): ProxyConfigMapCluster[] => {
-  try {
-    return JSON.parse(clustersJSON);
-  } catch {
-    return [];
-  }
-};
-
-const removeSecretFromClustersJSON = (clustersJSON: string, secret: OAuthSecret): string => {
-  let clusters = parseClustersJSON(clustersJSON);
-  clusters = clusters.filter((secretRef) => !isSameResource(secretRef, secret.metadata));
-  return JSON.stringify(clusters);
-};
-
-const addSecretToClustersJSON = (clustersJSON: string, secret: OAuthSecret): string => {
-  const clusters = parseClustersJSON(clustersJSON);
-  if (!clusters.find((secretRef) => isSameResource(secretRef, secret.metadata))) {
-    clusters.push({ name: secret.metadata.name, namespace: secret.metadata.namespace });
-  }
-  return JSON.stringify(clusters);
-};
 
 export const secretMatchesCredentials = (secret: OAuthSecret, apiUrl: string, token: string) =>
   secret.data.url === btoa(apiUrl) && secret.data.token === btoa(token);
