745dd0cc866a03bb535ba70cdbe3b88cb3cce732
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-03-16 17:41:01 -0400

    WIP: hide editor behind an advanced switch

--- src/components/ImportWizard/ReviewStep.tsx
+++ src/components/ImportWizard/ReviewStep.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import useSize from '@react-hook/size';
-import { TextContent, Text, MenuContent, MenuItem, MenuList, Alert } from '@patternfly/react-core';
+import {
+  TextContent,
+  Text,
+  MenuContent,
+  MenuItem,
+  MenuList,
+  Alert,
+  Switch,
+} from '@patternfly/react-core';
 import { CodeEditor, Language } from '@patternfly/react-code-editor';
 import spacing from '@patternfly/react-styles/css/utilities/Spacing/spacing';
 
@@ -17,13 +25,20 @@ type YamlFieldKey = keyof Pick<
 export const ReviewStep: React.FunctionComponent = () => {
   const forms = React.useContext(ImportWizardFormContext);
   const { pipelineName } = forms.pipelineSettings.values;
-
   const isStatefulMigration = forms.pvcSelect.values.selectedPVCs.length > 0;
+  const { stagePipeline, cutoverPipeline } = yamlToTektonResources(forms);
+
+  // TODO warn somehow if the user is going to override their manual edits here when they go to another step (use isTouched)? not sure how to do that if they use canJumpTo
+
+  // TODO take a look at onEditorDidMount in the PF examples, what's going on with that, how is it implemented?
+  // https://www.patternfly.org/v4/components/code-editor/
+  // TODO figure out what's going wrong when we try to set up monaco-editor-webpack-plugin, which we need for syntax highlighting
+
+  const [isAdvancedMode, setIsAdvancedMode] = React.useState(false);
+
   const yamlFieldKeys: YamlFieldKey[] = isStatefulMigration
     ? ['stagePipelineYaml', 'stagePipelineRunYaml', 'cutoverPipelineYaml', 'cutoverPipelineRunYaml']
     : ['cutoverPipelineYaml', 'cutoverPipelineRunYaml'];
-
-  // TODO warn somehow if the user is going to override their manual edits here when they go to another step (use isTouched)? not sure how to do that if they use canJumpTo
 
   const [selectedEditorKey, setSelectedEditorKey] =
     React.useState<YamlFieldKey>('cutoverPipelineYaml');
@@ -33,21 +48,19 @@ export const ReviewStep: React.FunctionComponent = () => {
     .map((fieldKey) => forms.review.fields[fieldKey].error?.message)
     .filter((err) => !!err);
 
-  // TODO take a look at onEditorDidMount in the PF examples, what's going on with that, how is it implemented?
-  // https://www.patternfly.org/v4/components/code-editor/
-  // TODO figure out what's going wrong when we try to set up monaco-editor-webpack-plugin, which we need for syntax highlighting
-
-  const { stagePipeline, cutoverPipeline } = yamlToTektonResources(forms);
-
   const errorContainerRef = React.useRef<HTMLDivElement>(null);
   const errorContainerHeight = useSize(errorContainerRef)[1];
 
   const scrollToEditor = () =>
     requestAnimationFrame(() => errorContainerRef.current?.scrollIntoView());
   const [hasTouchedEditor, setHasTouchedEditor] = React.useState(false);
-  const onVisualizationUpdate = (hasError: boolean) => {
-    if (hasTouchedEditor && !hasError) scrollToEditor();
+  const onVisualizationUpdate = () => {
+    if (hasTouchedEditor && isAdvancedMode) scrollToEditor();
   };
+
+  // TODO use the real metadata names for headings if present? helper function for that?
+  // TODO the spacing is all weird around the Switch?
+  // TODO details at the top of the Review step, see mockup
 
   return (
     <>
@@ -71,41 +84,50 @@ export const ReviewStep: React.FunctionComponent = () => {
         <Text component="h3">{isStatefulMigration ? `${pipelineName}-cutover` : pipelineName}</Text>
       </TextContent>
       <PipelineVisualizationWrapper pipeline={cutoverPipeline} onUpdate={onVisualizationUpdate} />
-      TODO: put below under an Advanced toggle
-      <SimpleSelectMenu<YamlFieldKey>
-        selected={selectedEditorKey}
-        setSelected={setSelectedEditorKey}
-        selectedLabel={selectedEditorFormField.schema.describe().label}
-        id="editor-select"
-        toggleProps={{ style: { width: '200px' } }}
-      >
-        <MenuContent>
-          <MenuList>
-            {yamlFieldKeys.map((fieldKey) => (
-              <MenuItem key={fieldKey} itemId={fieldKey}>
-                {forms.review.fields[fieldKey].schema.describe().label}
-              </MenuItem>
-            ))}
-          </MenuList>
-        </MenuContent>
-      </SimpleSelectMenu>
-      <div className={spacing.pbMd}>
-        <CodeEditor
-          key={selectedEditorKey}
-          isDarkTheme
-          isLineNumbersVisible
-          isLanguageLabelVisible
-          isMinimapVisible
-          code={selectedEditorFormField.value}
-          onChange={(value) => {
-            selectedEditorFormField.setValue(value);
-            selectedEditorFormField.setIsTouched(true);
-            setHasTouchedEditor(true);
-          }}
-          language={Language.yaml}
-          height={`${500 - errorContainerHeight}px`}
-        />
-      </div>
+      <Switch
+        id="advanced-switch"
+        label="View Pipeline and PipelineRun YAML files (advanced)"
+        isChecked={isAdvancedMode}
+        onChange={setIsAdvancedMode}
+      />
+      {isAdvancedMode ? (
+        <div>
+          <SimpleSelectMenu<YamlFieldKey>
+            selected={selectedEditorKey}
+            setSelected={setSelectedEditorKey}
+            selectedLabel={selectedEditorFormField.schema.describe().label}
+            id="editor-select"
+            toggleProps={{ style: { width: '200px' } }}
+          >
+            <MenuContent>
+              <MenuList>
+                {yamlFieldKeys.map((fieldKey) => (
+                  <MenuItem key={fieldKey} itemId={fieldKey}>
+                    {forms.review.fields[fieldKey].schema.describe().label}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </MenuContent>
+          </SimpleSelectMenu>
+          <div className={spacing.pbMd}>
+            <CodeEditor
+              key={selectedEditorKey}
+              isDarkTheme
+              isLineNumbersVisible
+              isLanguageLabelVisible
+              isMinimapVisible
+              code={selectedEditorFormField.value}
+              onChange={(value) => {
+                selectedEditorFormField.setValue(value);
+                selectedEditorFormField.setIsTouched(true);
+                setHasTouchedEditor(true);
+              }}
+              language={Language.yaml}
+              height={`${550 - errorContainerHeight}px`}
+            />
+          </div>
+        </div>
+      ) : null}
       <div ref={errorContainerRef} className={yamlErrors.length > 0 ? spacing.pbMd : ''}>
         {yamlErrors.length > 0 ? (
           <Alert isInline variant="danger" title="Invalid YAML">
