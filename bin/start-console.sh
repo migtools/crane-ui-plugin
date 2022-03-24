b768a2fc4a214f1f8c5ef4df73cee515c9dddb04
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-03-24 16:03:21 -0400

    Trying a different order?

--- bin/start-console.sh
+++ bin/start-console.sh
@@ -16,7 +16,8 @@ SECRET_SERVICE_API_PATH="/api/proxy/plugin/crane-ui-plugin/secret-service/"
 SECRET_SERVICE_ENDPOINT="https://$(oc get route -n openshift-migration secret-service -o go-template='{{ .spec.host }}')"
 
 PLUGIN_PROXY_JSON_FMT='{ "services": [ { "consoleAPIPath": "%s", "endpoint": "%s", "authorize": %s }, { "consoleAPIPath": "%s", "endpoint": "%s", "authorize": %s } ] }'
-PLUGIN_PROXY_JSON=$(printf "$PLUGIN_PROXY_JSON_FMT" "$CRANE_PROXY_API_PATH" "$CRANE_PROXY_ENDPOINT" "false" "$SECRET_SERVICE_API_PATH" "$SECRET_SERVICE_ENDPOINT" "true" )
+# PLUGIN_PROXY_JSON=$(printf "$PLUGIN_PROXY_JSON_FMT" "$CRANE_PROXY_API_PATH" "$CRANE_PROXY_ENDPOINT" "false" "$SECRET_SERVICE_API_PATH" "$SECRET_SERVICE_ENDPOINT" "false" )
+PLUGIN_PROXY_JSON=$(printf "$PLUGIN_PROXY_JSON_FMT" "$SECRET_SERVICE_API_PATH" "$SECRET_SERVICE_ENDPOINT" "true" "$CRANE_PROXY_API_PATH" "$CRANE_PROXY_ENDPOINT" "false" )
 
 echo "\nUsing --plugin-proxy:\n  $PLUGIN_PROXY_JSON\n"
 
