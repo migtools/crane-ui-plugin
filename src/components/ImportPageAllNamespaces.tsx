5e7daa28eeb9ec6c05e1aff5675a73d95c924b5b
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-04-05 12:57:39 -0400

    WIP: redirect on all-namespaces?

---
+++ src/components/ImportPageAllNamespaces.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Redirect } from 'react-router-dom';
+
+const ImportPageAllNamespaces: React.FunctionComponent = () => (
+  <Redirect to="/add/all-namespaces" />
+);
+
+export default ImportPageAllNamespaces;
