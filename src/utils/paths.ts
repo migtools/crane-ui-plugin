c3a629fb87193db501c31a40aea22c4fbe3510d7
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-07-14 12:04:05 -0400

    Attempt to add /k8s to the path to add a project switcher bar

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,10 +1,12 @@
 import { pipelineRunGVK } from 'src/api/queries/pipelines';
 import { PipelineRunKind } from 'src/reused/pipelines-plugin/src/types';
 
-export const appImportWizardUrl = (namespace: string) => `/app-imports/new/ns/${namespace}`;
+export const appImportWizardUrl = (namespace: string) => `/import-application/ns/${namespace}`;
 
 export const appImportsPageUrl = (namespace: string, pipelineGroupName?: string) =>
-  `/app-imports/ns/${namespace}${pipelineGroupName ? `/${pipelineGroupName}` : ''}`;
+  `/k8s/ns/${namespace}/app-imports${pipelineGroupName ? `/${pipelineGroupName}` : ''}`;
+
+export const appImportsAllNamespacesUrl = () => '/k8s/all-namespaces/app-imports';
 
 export const pipelineRunUrl = (namespace: string, pipelineRun: PipelineRunKind) => {
   const { group, version, kind } = pipelineRunGVK;
