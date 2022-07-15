cf5db75920ef06329eb560d2778043a8d544c324
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-07-15 16:55:32 -0400

    Add delete confirm modal

--- src/components/AppImports/PipelineGroupKebabMenu.tsx
+++ src/components/AppImports/PipelineGroupKebabMenu.tsx
@@ -6,6 +6,7 @@ import { CranePipelineGroup } from 'src/api/types/CranePipeline';
 import { useDeletePipelineMutation } from 'src/api/queries/pipelines';
 import { useNamespaceContext } from 'src/context/NamespaceContext';
 import { pipelinesListUrl } from 'src/utils/paths';
+import { ConfirmModal } from 'src/common/components/ConfirmModal';
 
 interface PipelineGroupKebabMenuProps {
   pipelineGroup: CranePipelineGroup;
@@ -31,33 +32,50 @@ export const PipelineGroupKebabMenu: React.FunctionComponent<PipelineGroupKebabM
     onFocus('toggle-id-app-kebab');
   };
 
+  const [isConfirmDeleteModalOpen, setIsConfirmDeleteModalOpen] = React.useState(false);
+
   return (
-    <Dropdown
-      onSelect={onAppKebabSelect}
-      toggle={<KebabToggle onToggle={toggleAppKebabOpen} id="toggle-id-app-kebab" />}
-      isOpen={isAppKebabOpen}
-      isPlain
-      position="right"
-      dropdownItems={[
-        <DropdownItem
-          key="app-delete"
-          component="button"
-          onClick={() =>
-            // TODO add a confirmation dialog!
-            deletePipelineMutation.mutate(pipelineGroup.pipelines.cutover)
-          }
-          isDisabled={deletePipelineMutation.isLoading}
-        >
-          Delete
-        </DropdownItem>,
-        <DropdownItem
-          key="app-view-pipelies"
-          component="button"
-          onClick={() => history.push(pipelinesListUrl(namespace, pipelineGroup.name))}
-        >
-          View pipelines
-        </DropdownItem>,
-      ]}
-    />
+    <>
+      <Dropdown
+        onSelect={onAppKebabSelect}
+        toggle={<KebabToggle onToggle={toggleAppKebabOpen} id="toggle-id-app-kebab" />}
+        isOpen={isAppKebabOpen}
+        isPlain
+        position="right"
+        dropdownItems={[
+          <DropdownItem
+            key="app-delete"
+            component="button"
+            onClick={() => setIsConfirmDeleteModalOpen(true)}
+            isDisabled={deletePipelineMutation.isLoading}
+          >
+            Delete
+          </DropdownItem>,
+          <DropdownItem
+            key="app-view-pipelies"
+            component="button"
+            onClick={() => history.push(pipelinesListUrl(namespace, pipelineGroup.name))}
+          >
+            View pipelines
+          </DropdownItem>,
+        ]}
+      />
+      <ConfirmModal
+        title="Delete pipelines and history?"
+        body={
+          <>
+            This will delete all Pipelines, PipelineRuns and Secrets that were created with the
+            group name <strong>&quot;{pipelineGroup.name}&quot;</strong>. All related logs and
+            history will be lost.
+          </>
+        }
+        confirmButtonText="Delete"
+        isOpen={isConfirmDeleteModalOpen}
+        toggleOpen={() => setIsConfirmDeleteModalOpen(!isConfirmDeleteModalOpen)}
+        mutateFn={() => deletePipelineMutation.mutate(pipelineGroup.pipelines.cutover)}
+        mutateResult={deletePipelineMutation}
+        errorText="Cannot delete resources"
+      />
+    </>
   );
 };
