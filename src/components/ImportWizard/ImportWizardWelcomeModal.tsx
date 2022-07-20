6ff9c4423d140609a2c94aa90fca51cbb6e4e8b4
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-07-20 10:41:42 -0400

    Separate paragraphs

--- src/components/ImportWizard/ImportWizardWelcomeModal.tsx
+++ src/components/ImportWizard/ImportWizardWelcomeModal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Modal, ModalProps } from '@patternfly/react-core';
+import { Button, Modal, ModalProps, TextContent, Text } from '@patternfly/react-core';
 import { appImportsPageUrl, pipelinesListUrl } from 'src/utils/paths';
 import { useNamespaceContext } from 'src/context/NamespaceContext';
 
@@ -21,11 +21,17 @@ export const ImportWizardWelcomeModal: React.FunctionComponent<ImportWizardWelco
       ]}
       {...props}
     >
-      This wizard will generate OpenShift Pipelines and pre-populated PipelineRuns for importing a
-      manually deployed application on another cluster to an automated GitOps workflow. The{' '}
-      <Link to={appImportsPageUrl(namespace)}>Application Imports</Link> page can be used to view
-      status and take actions on these pipelines, and they can also be seen in more detail on the{' '}
-      <Link to={pipelinesListUrl(namespace)}>Pipelines</Link> page.
+      <TextContent>
+        <Text component="p">
+          This wizard will generate OpenShift Pipelines and pre-populated PipelineRuns for importing
+          a manually deployed application on another cluster to an automated GitOps workflow.
+        </Text>
+        <Text component="p">
+          The <Link to={appImportsPageUrl(namespace)}>Application Imports</Link> page can be used to
+          view status and take actions on these pipelines, and they can also be seen in more detail
+          on the <Link to={pipelinesListUrl(namespace)}>Pipelines</Link> page.
+        </Text>
+      </TextContent>
     </Modal>
   );
 };
