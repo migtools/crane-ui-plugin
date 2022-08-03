e7a222d8869bb4467ef288d775cf6ddb53506634
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-08-03 13:42:38 -0400

    Remove tooltip from status link after feedback

--- src/components/ImportedApps/PipelineRunStatusLink.tsx
+++ src/components/ImportedApps/PipelineRunStatusLink.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Tooltip } from '@patternfly/react-core';
 import { StatusComponent } from '@openshift-console/dynamic-plugin-sdk';
 import { pipelineRunStatus } from 'src/reused/pipelines-plugin/src/utils/pipeline-filter-reducer';
 import { CranePipelineRun } from 'src/api/types/CranePipeline';
@@ -20,14 +19,12 @@ export const PipelineRunStatusLink: React.FunctionComponent<PipelineRunStatusLin
   const { namespace } = useValidatedNamespace();
   const action = resourceActionToString(pipelineRun, true);
   return (
-    <Tooltip content="Detailed import status is found under Pipelines in the navigation">
-      <Link to={pipelineRunUrl(namespace, pipelineRun)}>
-        <StatusComponent
-          className="crane-plr-status"
-          status={pipelineRunStatus(pipelineRun) || ''}
-          title={`${pipelineRunStatus(pipelineRun) || ''}${showAction ? ` ${action}` : ''}`}
-        />
-      </Link>
-    </Tooltip>
+    <Link to={pipelineRunUrl(namespace, pipelineRun)}>
+      <StatusComponent
+        className="crane-plr-status"
+        status={pipelineRunStatus(pipelineRun) || ''}
+        title={`${pipelineRunStatus(pipelineRun) || ''}${showAction ? ` ${action}` : ''}`}
+      />
+    </Link>
   );
 };
