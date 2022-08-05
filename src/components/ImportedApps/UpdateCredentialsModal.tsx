c9e0ad2c1e9979fd3218095c9d6e96648708922c
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-08-05 16:20:58 -0400

    Remove stale TODOs

--- src/components/ImportedApps/UpdateCredentialsModal.tsx
+++ src/components/ImportedApps/UpdateCredentialsModal.tsx
@@ -9,15 +9,6 @@ interface UpdateCredentialsModalProps {
   onClose: () => void;
 }
 
-// TODO: add field for pasting source cluster token, and repeat warning from first wizard step
-// TODO: validate before submitting? that's tricky... what if it's running (probably need to disable this while it's running)
-//       - maybe store the existing source token before letting the user edit it, and if they cancel, restore it on the secret before closing the modal?
-//       - or maybe create a separate temporary secret just for validation, and then delete it when we update the original? i like that more.
-// TODO: on submit, patch the source cluster secret with the new token, and patch the target cluster secret via crane-secret-service (noop? how to get that to just patch the token?)
-// TODO: when user clicks Stage or Cutover, somehow test the source and target tokens and prompt the user to reset them?
-//       - start with just a warning followed by opening this modal?
-//       - maybe instead: in the Stage or Cutover confirm modals themselves, add a checkbox for "update oauth tokens before starting" that defaults to checked? expand under it to show the body contents of this modal?
-//       - abstract just the fields into `UpdateCredentialsForm`? How to reuse the mutation itself? probably factor out into a shared useUpdateCredentialsMutation hook
 export const UpdateCredentialsModal: React.FunctionComponent<UpdateCredentialsModalProps> = ({
   pipelineGroup,
   isOpen,
